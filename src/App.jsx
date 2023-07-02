c8fe7e8c5dc9620eaafa110e2d58d68e94e55b69
WafaBenyettou <benwafa951@gmail.com>
2023-07-02 15:03:56 +0100

    project struct

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import React from 'react';
 
+import Article from './components/article/Article.jsx';
+import Brand from './components/brand/Brand.jsx';
+import CTA from './components/cta/CTA.jsx';
+import Features from './components/features/Features.jsx';
+import Navbar from './components/navbar/Navbar.jsx';
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>
-       <h1>React + vite</h1>
-   </div>
+     <Article />
+      <Brand />
+      <CTA />
+      <Features />
+      <Navbar />
+      
+
     </>
   )
 }
--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,1 +1,10 @@
-r
+import React from 'react';
+import './article.css';
+
+const Article = () => {
+  return (
+    <div>Article</div>
+  )
+}
+
+export default Article
