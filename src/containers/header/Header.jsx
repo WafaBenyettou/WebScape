67fea2a2aecf3e14dd84b0bff16208b50303f986
WafaBenyettou <benwafa951@gmail.com>
2023-07-02 21:59:13 +0100

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import avatar from "../../assets/home-avatar.png";
+import people from "../../assets/people.png";
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
 
-export default Header
+const Header = () => (
+  <div className="webscape-header section-padding" id="home">
+    <div className="webscape-header-content">
+      <h1 className="gradient-text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+
+      <div className="webscape-header-content-input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="webscape-header-content-social">
+        <img src={people} />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+    </div>
+
+    <div className="webscape-header-image">
+      <img src={avatar} />
+    </div>
+  </div>
+);
+
+export default Header;
