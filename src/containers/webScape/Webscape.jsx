a84fa754f27e7ceeba32358327e5cfbf48384f1a
WafaBenyettou <benwafa951@gmail.com>
2023-07-02 23:27:11 +0100

    about

--- src/containers/webScape/Webscape.jsx
+++ src/containers/webScape/Webscape.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './webscape.css'
+import Features from '../../components/features/Features'
 
 const Webscape = () => {
   return (
-    <div>Webscape</div>
+    <div className='webscape-webscape section-margin' id='webscape'>
+      <div className='webscape-webscape-features'>
+        <Features />
+        </div>
+        <div className='webscape-webscape-heading'>
+          <h1 className='gradient-text'>One click away from your dreams...</h1>
+          <p>Discover out blog</p>
+        </div>
+
+        <div className='webscape-webscape-container'>
+          <Features />
+          <Features />
+          <Features />
+        </div>
+    </div>
   )
 }
 
