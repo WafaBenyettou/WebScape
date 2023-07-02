a2b90f9c5896b7c6ec352cf263701991cba30cf8
WafaBenyettou <benwafa951@gmail.com>
2023-07-02 15:18:57 +0100

    project struct

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 
+// imported components
 import Article from './components/article/Article.jsx';
 import Brand from './components/brand/Brand.jsx';
 import CTA from './components/cta/CTA.jsx';
 import Features from './components/features/Features.jsx';
 import Navbar from './components/navbar/Navbar.jsx';
+
+//imported containers
+import Blog from './containers/blog/Blog.jsx';
+import Footer from './containers/footer/Footer.jsx';
+import Feature from './containers/feature/Feature.jsx';
+import Header from './containers/header/Header.jsx';
+import Possibility from './containers/possibility/Possibility.jsx';
+import Webscape from './containers/webScape/Webscape.jsx';
+
+//imported styles
 import './App.css'
+
 
 function App() {
   return (
     <>
 
-    
-      <Navbar />
-      <Article />
-      <Brand />
-      <CTA />
-      <Features />
+       <h1>Hello world!</h1>
 
 
 
