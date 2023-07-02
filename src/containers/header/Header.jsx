cbf46896f97d59957bc26d6a02ac9dc33b8f1dfe
WafaBenyettou <benwafa951@gmail.com>
2023-07-02 23:03:25 +0100

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,12 +3,18 @@ import "./header.css";
 import avatar from "../../assets/home-avatar.png";
 import icon from "../../assets/icon.png";
 
-
 const Header = () => (
   <div className="webscape-header section-padding" id="home">
     <div className="webscape-header-content">
-      <h1 className="gradient-text">Explore our latest inventions with &apos; WebScape &apos; !</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident dolore saepe exercitationem illo, et enim rerum reprehenderit optio doloremque, excepturi illum magnam labore assumenda ducimus ad repellat atque consectetur at!</p>
+      <h1 className="gradient-text">
+        Explore our latest inventions with &apos; WebScape &apos; !
+      </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident
+        dolore saepe exercitationem illo, et enim rerum reprehenderit optio
+        doloremque, excepturi illum magnam labore assumenda ducimus ad repellat
+        atque consectetur at!
+      </p>
 
       <div className="webscape-header-content-input">
         <input type="email" placeholder="Your Email Address" />
