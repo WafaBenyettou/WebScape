797f0e5c2e9c31e406b0791780548ea30ba5d533
WafaBenyettou <benwafa951@gmail.com>
2023-07-02 23:20:27 +0100

    brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import './brand.css';
+import React from "react";
+import "./brand.css";
+import logo1 from "../../assets/logo1.png";
+import logo2 from "../../assets/logo2.png";
+import logo3 from "../../assets/logo3.png";
+import logo4 from "../../assets/logo4.png";
+import logo5 from "../../assets/logo5.png";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="webscape-brand section-padding">
+      <div>
+        <img src={logo1} />
+      </div>
+      <div>
+        <img src={logo2} />
+      </div>
+      <div>
+        <img src={logo5} />
+      </div>
+      <div>
+        <img src={logo3} />
+      </div>
+      <div>
+        <img src={logo4} />
+      </div>
+     
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
