b0acbcf3520e92f1de74ea772b8dcc228ec01b1a
WafaBenyettou <benwafa951@gmail.com>
2023-07-02 16:28:02 +0100

    navbar css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/WebScape_white-logo.png'
 import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='webscape-navbar'>
+        <div className='webscape-navbar-links'>
+            <div className='webscape-navbar-links-logo'>
+               <img src={logo} alt='logo'/>
+            </div>
+            <div className='webscape-navbar-links-container'>
+                <p><a href='#home'>Home</a></p>
+                <p><a href='#webscape'>About</a></p>
+                <p><a href='#possibility'>Possibility</a></p>
+                <p><a href='#features'>Features</a></p>
+                <p><a href='#blog'>Blog</a></p>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
