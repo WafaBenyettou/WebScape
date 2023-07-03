65c35db6aafded84774078818ee276d4f0bd64e3
WafaBenyettou <benwafa951@gmail.com>
2023-07-03 14:42:56 +0100

    Blog container

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './blog.css'
+import Article from '../../components/article/Article'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='webscape-blog section-padding' id='blog'>
+          <div className='webscape-blog-heading'>
+            <h1 className='gradient-text'>Read our blog to get update</h1>
+          </div>
+
+          <div className='webscape-blog-container'>
+            <div className='webscape-blog-container-groupA'>
+              <Article />
+            </div>
+            <div className='webscape-blog-container-groupB'>
+              <Article />
+              <Article />
+              <Article />
+              <Article />
+            </div>
+          </div>
+    </div>
   )
 }
 
