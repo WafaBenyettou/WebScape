32086ebc96366d78c516e1f3312aa411301772cd
WafaBenyettou <benwafa951@gmail.com>
2023-07-03 14:37:30 +0100

    cta

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import './cta.css'
+import React from "react";
+import "./cta.css";
 
 const CTA = () => {
   return (
     <div className="webscape-cta">
-    <div className="webscape-cta-content">
-      <p>Request Early Access to Get Started</p>
-      <h3>Register Today & start exploring the endless possibilities.</h3>
+      <div className="webscape-cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="webscape-cta-btn">
+        <button type="button">Get Started</button>
+      </div>
     </div>
-    <div className="webscape-cta-btn">
-      <button type="button">Get Started</button>
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
