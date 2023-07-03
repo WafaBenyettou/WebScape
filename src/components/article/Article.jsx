ce75e91c81428727874a4357fbc779c833062d2d
WafaBenyettou <benwafa951@gmail.com>
2023-07-03 15:01:18 +0100

    Article and blog

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import './article.css';
+import React from "react";
+import "./article.css";
 
-const Article = ({img}) => {
+const Article = ({ img, date, title }) => {
   return (
-    <div className='webscape-blog-container-article'> 
-    <div className='webscape-blog-container-article-image'>
-      <img src={img} alt='article' />
+    <div className="webscape-blog-container-article">
+      <div className="webscape-blog-container-article-image">
+        <img src={img} alt="article" />
+      </div>
+      <div className="webscape-blog-container-article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read more...</p>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Article
+export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import './blog.css'
 import Article from '../../components/article/Article'
 
+import blog1 from '../../assets/blog1.png'
+import blog2 from '../../assets/blog2.png'
+import blog3 from '../../assets/blog3.png'
+import blog4 from '../../assets/blog4.png'
+import blog5 from '../../assets/blog5.png'
+
 const Blog = () => {
   return (
     <div className='webscape-blog section-padding' id='blog'>
@@ -11,13 +17,13 @@ const Blog = () => {
 
           <div className='webscape-blog-container'>
             <div className='webscape-blog-container-groupA'>
-              <Article />
+              <Article img={blog5} date="Jan 16, 2002" title="Mastering CSS Grid: A Guide for Web Developers" />
             </div>
             <div className='webscape-blog-container-groupB'>
-              <Article />
-              <Article />
-              <Article />
-              <Article />
+              <Article img={blog1} date="Sep 09, 2001" title="The Rise of Progressive Web Apps: A Game Changer in Web Development" />
+              <Article img={blog2} date="Oct 21, 2018" title="5 Tips for Writing Clean and Maintainable HTML Code" />
+              <Article img={blog3} date="Jun 11, 2023" title="JavaScript Framework Showdown: React vs. Vue vs. Angular" />
+              <Article img={blog4} date="Mar 15, 2019" title="Responsive Web Design: Creating Websites for All Devices" />
             </div>
           </div>
     </div>
