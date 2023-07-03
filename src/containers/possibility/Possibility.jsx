638a9361c6cd38b601491a4b12d1cd84bc2af67c
WafaBenyettou <benwafa951@gmail.com>
2023-07-03 14:26:20 +0100

    possibility container

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './possibility.css'
+import avatar from '../../assets/pos-avatar.png'
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='webscape-possibility section-padding' id='possibility'>
+      <div className='webscape-possibility-image'>
+        <img src={avatar} alt='pos avatar' />
+      </div>
+      <div className='webscape-possibility-content'>
+        <h4>What is the possibility of success?</h4>
+        <h1 className='gradient-text'>Discover the possibilities with us Today</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident dolore saepe exercitationem illo, et enim rerum reprehenderit optio doloremque, excepturi illum magnam labore assumenda ducimus ad repellat atque consectetur at!</p>
+        <h4>Unlock the pro experience !</h4>
+        </div>
+    </div>
   )
 }
 
